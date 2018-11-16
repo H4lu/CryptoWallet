b222e528ac3f139eafefc5dd2241160c758da250
Kuku <A6401142@gmail.com>
2018-11-16 17:51:26 +0300

    day 4

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,103 +1,92 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { Table } from './Table'
-import { connect } from 'react-redux'
-import { ETHEREUM_PATH, BITCOIN_PATH, LITECOIN_PATH } from '../core/paths'
+import {Link} from 'react-router-dom'
+import {Table} from './Table'
+import {connect} from 'react-redux'
+import {ETHEREUM_PATH, BITCOIN_PATH, LITECOIN_PATH} from '../core/paths'
+
 class MainContent extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props)
+    constructor(props: any) {
+        super(props)
 
-    this.handleClick = this.handleClick.bind(this)
-  }
-  handleClick () {
-    this.props.refresh()
-  }
+        this.handleClick = this.handleClick.bind(this)
+    }
 
-  render () {
-    return (
-    <div className = 'main'>
-      <div className = 'main-content'>
-          <div className = 'currency-block'>
-            <header className = 'currencies-header'>Cryptocurrency: </header>
-          <div className = 'currencies-container'>
-            <Link to = '/btc-window' className = 'card-container'>
-              <div className = 'card-upper-block'>
-                <img src = {BITCOIN_PATH}/>
-                <p className = 'currency-name'> Bitcoin</p>
-              </div>
-              <hr/>
-              <div className = 'card-bottom-block'>
-              <div>
-                   <p className = 'currency-amount-crypto text-inline'> {this.props.btcBalance}</p><p className = 'currency-short-name text-inline'>BTC</p>
-                  </div>
-                  <div className = 'wrap'>
-                    {(this.props.btcHourChange > 0) ? (
-                      <p className = 'positive-percentage text-inline'>{this.props.btcHourChange}%</p>
-                    ) : (
-                      <p className = 'negative-percentage text-inline'>{this.props.btcHourChange}%</p>
-                    )}
-                   <p className = 'currency-amount-fiat text-inline'>{this.props.btcPrice}$</p>
-                   </div>
-              </div>
-            </Link>
-            <Link to = '/eth-window' className = 'card-container'>
-              <div className = 'card-upper-block'>
-                <img src = {ETHEREUM_PATH}/>
-                <p className = 'currency-name'>Ethereum</p>
-              </div>
-              <hr/>
-              <div className = 'card-bottom-block'>
-                <div>
-                  <p className = 'currency-amount-crypto text-inline'> {this.props.ethBalance}</p><p className = 'currency-short-name text-inline'>ETH </p>
-                </div>
-                <div className = 'wrap'>
-                  {(this.props.ethHourChange > 0) ? (
-                    <p className = 'positive-percentage text-inline'>{this.props.ethHourChange}%</p>
-                  ) : (
-                    <p className = 'negative-percentage text-inline'>{this.props.ethHourChange}%</p>
-                  )}
-                  <p className = 'currency-amount-fiat text-inline'>{this .props.ethPrice}$</p>
-                </div>
-              </div>
-            </Link>
-            <Link to = '/ltc-window' className = 'card-container'>
-              <div className = 'card-upper-block'>
-                <img src = {LITECOIN_PATH}/>
-                <p className = 'currency-name'>Litecoin</p>
-              </div>
-              <hr/>
-              <div className = 'card-bottom-block'>
-                <div>
-                   <p className = 'currency-amount-crypto text-inline'> {this.props.ltcBalance}</p><p className = 'currency-short-name text-inline'>LTC </p>
-                </div>
-                <div className = 'wrap'>
+    handleClick() {
+        this.props.refresh()
+    }
 
-                {(this.props.ltcHourChange > 0) ? (
-                  <p className = 'positive-percentage text-inline'>{this.props.ltcHourChange}%</p>
-                ) : (
-                  <p className = 'negative-percentage text-inline'>{this.props.ltcHourChange}%</p>
-                )}
-                <p className = 'currency-amount-fiat'>{this.props.ltcPrice}$</p>
+    render() {
+        return (
+            <div className='main'>
+                <div className='main-content'>
+                    <div className='currency-block'>
+                        <div className='vert'>
+                            <header className='currencies-header'>My wallets</header>
+                            <div className='cryptocurrency'>
+                                <div className='cryptocurrency_flex'>
+                                    <div className='column_1'>
+                                        <div className='column_BTC_ETH'>
+                                            <div className='but_BTC'>
+                                                <Link to='/btc-window' className='card-container'>
+                                                    <div className='img_BTC'>
+                                                    <p className='balance'> {this.props.btcBalance}</p>
+                                                    <p className='fiat'>{this.props.btcPrice}</p>
+                                                    </div>
+                                                </Link>
+                                            </div>
+                                            <div className='but_ETH'>
+                                                <Link to='/eth-window' className='card-container'>
+                                                    <div className='img_ETH'>
+                                                    <p className='balance'> {this.props.ethBalance}</p>
+                                                    <p className='fiat'>{this.props.ethPrice}</p>
+                                                    </div>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='column_2'>
+                                        <div className='column_LTC_XRP'>
+                                            <div className='but_LTC'>
+                                                <Link to='/ltc-window' className='card-container'>
+                                                    <div className='img_LTC'>
+                                                    <p className='balance'> {this.props.ltcBalance}</p>
+                                                    <p className='fiat'>{this.props.ltcPrice}</p>
+                                                    </div>
+                                                </Link>
+                                            </div>
+                                            <div className='but_XRP'>
+                                                <Link to='/btc-window' className='card-container'>
+                                                    <div className='img_XRP'>
+                                                    <p className='balance'> {this.props.btcBalance}</p>
+                                                    <p className='fiat'>{this.props.btcPrice}</p>
+                                                    </div>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='Block_explorer'/>
+                        </div>
+                    </div>
+                    <div className='transaction_info'>
+
+                    </div>
                 </div>
-                </div>
-            </Link>
-           </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+            </div>
+        )
+    }
 }
 
 function mapStateToProps(state: any, IMainContentProps) {
-  console.log('MY STATE: ' + state)
-  console.log(state)
-  console.log(JSON.stringify(state))
-  console.log(IMainContentProps)
-  console.log('BALANCE STATE: ' + state.getBalance)
-  return {
-    balance: state.getBalance
-  }
+    console.log('MY STATE: ' + state)
+    console.log(state)
+    console.log(JSON.stringify(state))
+    console.log(IMainContentProps)
+    console.log('BALANCE STATE: ' + state.getBalance)
+    return {
+        balance: state.getBalance
+    }
 }
 
 export default connect(mapStateToProps)(MainContent)
