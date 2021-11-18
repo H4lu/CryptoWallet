c20a78d6f9ef0a5ac31f4ead74e7e726633d2e62
H4lu <halurecht@gmail.com>
2021-11-18 10:58:51 +0300

    fix webpack rules

--- webpack.rules.js
+++ webpack.rules.js
@@ -1,37 +1,39 @@
 module.exports = [
-  // // Add support for native node modules
-  // {
-  //   test: /\.node$/,
-  //   use: 'node-loader',
-  // },
-  // {
-  //   test: /\.(m?js|node)$/,
-  //   parser: { amd: false },
-  //   use: {
-  //     loader: '@marshallofsound/webpack-asset-relocator-loader',
-  //     options: {
-  //       outputAssetBase: 'native_modules',
-  //     },
-  //   },
-  // },
-  {
-    test: /\.tsx?$/,
-    exclude: /(node_modules|\.webpack)/,
-    use: {
-      loader: 'ts-loader',
-      options: {
-        onlyCompileBundledFiles: true
-      }
-    }
-  },  
+    // // Add support for native node modules
+    // {
+    //   test: /\.node$/,
+    //   use: 'node-loader',
+    // },
+    // {
+    //   test: /\.(m?js|node)$/,
+    //   parser: { amd: false },
+    //   use: {
+    //     loader: '@marshallofsound/webpack-asset-relocator-loader',
+    //     options: {
+    //       outputAssetBase: 'native_modules',
+    //     },
+    //   },
+    // },
     {
-      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-      use : [
-          {loader: 'file-loader',
-           options: {
-               name : 'assets/[name].[contenthash].[ext]',
-          } 
-          }
-      ]
+        test: /\.tsx?$/,
+        exclude: /(node_modules|\.webpack)/,
+        use: {
+            loader: 'ts-loader',
+            options: {
+                onlyCompileBundledFiles: true,
+                transpileOnly: true
+            }
+        }
+    },
+    {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: 'assets/[name].[contenthash].[ext]',
+                }
+            }
+        ]
     }
 ];
